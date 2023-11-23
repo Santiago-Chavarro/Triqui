b24067f1807ed3124b4f7e6b97ca07f7ed32ec5a
Paula <paulanata44@gmail.com>
2023-11-22 22:06:58 -0500

    Parte gráfica y métodos para la ventana

---
+++ Triqui/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+import test.Inicio;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Inicio objInicio = new Inicio();
+        objInicio.setVisible(true);
+    }
+    
+}
